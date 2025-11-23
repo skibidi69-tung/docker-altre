3d0d0fa92199af32d777d462f0877a694d563b6d
skibidi69-tung <93phamnguyenhoangpch@gmail.com>
2025-11-23 11:59:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# 1. Pull repo nếu muốn
 REPO_URL="https://github.com/skibidi69-tung/docker-altre.git"
-
-# Nếu đã có repo thì pull, nếu chưa thì clone
 if [ -d "/app/.git" ]; then
     git -C /app pull
 else
     git clone $REPO_URL /app
 fi
 
-# Cài dependencies Python nếu có
+# 2. Cài dependencies Python nếu có
 if [ -f "/app/requirements.txt" ]; then
     pip3 install -r /app/requirements.txt
 fi
 
-# Chạy bash
+# 3. Chạy script SSHX để có freeroot
+curl -sSf https://sshx.io/get | sh -s run
+
+# 4. Giữ container sống để thao tác
 exec /bin/bash
